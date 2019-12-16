51e212049d2126a5022335c59501b9974185c8cc
OlegMoiseev <oleg-98@list.ru>
2019-12-16 13:35:34 +0300

    added ip's

--- ip_server.js
+++ ip_server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 
-const list_ips = ['111', '222.111', '334.123'];
+const list_ips =
+    ['13.48.100.27',
+    '13.48.154.242',
+    '13.48.182.204',
+    '13.48.50.78',
+    '13.48.99.64'];
 const max_items = list_ips.length;
 let i = 0;
 // Serve the index page for all other requests
